cbbd55d4b7a38ba43d305574256c3f23005d2d0d
ChrisCN97 <874147338@qq.com>
2020-07-14 16:33:30 +0800

    fix some bug

--- code/txt2csv.py
+++ code/txt2csv.py
@@ -3,6 +3,7 @@ import csv
 #neo4j-admin import --database=CN-DBpedia.db --nodes="../import/node.csv" --relationships="../import/relation.csv" --multiline-fields=true
 
 if __name__ == "__main__":
+    # 改为cn-dbpedia的数据
     dataPath = "../data/sample.txt"
 
     resourceFile = open(dataPath, encoding='utf-8')
@@ -11,7 +12,8 @@ if __name__ == "__main__":
     nodeCsv = csv.writer(nodeFile)
     relationCsv = csv.writer(relationFile)
 
-    nodeCsv.writerow([":ID", "name"])
+    # node.csv中最好有:LABEL属性
+    nodeCsv.writerow([":ID", "name", ":LABEL"])
     # 不能将relation的名字直接设置为TYPE，不然TYPE会达到数量上限
     relationCsv.writerow([":START_ID", ":END_ID", "name", ":TYPE"])
     nodeDic = dict()
@@ -20,15 +22,19 @@ if __name__ == "__main__":
     line = resourceFile.readline()
     while line is not None and line != '':
         n1, r, n2 = line.split('\t')
+        # 该图谱最长的节点name长达19759，后期建立索引会失败，可以在这里把过长的节点或关系剔除
+        if len(n1)>15 or len(n2)>15 or len(r)>15:
+            line = resourceFile.readline()
+            continue
         if n1 not in nodeDic:
             nodeDic[n1] = num
-            nodeCsv.writerow([num, n1])
+            nodeCsv.writerow([num, n1, "node"])
             num += 1
         if n2 not in nodeDic:
             nodeDic[n2] = num
-            nodeCsv.writerow([num, n2])
+            nodeCsv.writerow([num, n2, "node"])
             num += 1
-        relationCsv.writerow([nodeDic[n1], nodeDic[n2], r, "1"])
+        relationCsv.writerow([nodeDic[n1], nodeDic[n2], r, "relation"])
 
         line = resourceFile.readline()
 
